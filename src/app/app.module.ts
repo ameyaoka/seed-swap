c37b7707d00bf720f49f36a336c71af90bd2589b
ameyaoka <ameyaoka444@gmail.com>
2021-04-11 20:01:51 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeedListComponent } from './seed-list/seed-list.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import {RouterModule,Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+	{path : 'browse-seed' , component : SeedListComponent },
+	
+	{path : 'sign-up' , component : CreateAccountComponent  },
+];	
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+    	appRoutes,
+	{enableTracing: true} // debugging purpose only
+	)
   ],
+  
+
+ 
+
+
+  declarations : [
+  	AppComponent,
+	SeedListComponent,
+	CreateAccountComponent ,
+],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
